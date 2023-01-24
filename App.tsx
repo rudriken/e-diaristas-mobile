462664aa2764e08ed4d09972eef3e99f9495db32
Rodrigo <rod_igomail@hotmail.com>
2023-01-24 10:34:15 -0300

    finalizada aula 'Navegação'

--- App.tsx
+++ App.tsx
@@ -1,22 +1,7 @@
 import "logica/servicos/RNEnv";
 import "logica/servicos/RNPolyfills";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Roteador from "visual/roteador/Roteador";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+	return <Roteador />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
---
+++ src/visual/roteador/Roteador.tsx
@@ -0,0 +1,38 @@
+import { View, Text, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Logo from "@assets/img/logos/e-diaristas-logo.png";
+
+const Pilha = createStackNavigator();
+
+function Index() {
+	return (
+		<View>
+			<Text>Tela Index</Text>
+		</View>
+	);
+}
+
+export default function Roteador() {
+	return (
+		<NavigationContainer>
+			<Pilha.Navigator
+				screenOptions={{
+					headerTitleAlign: "center",
+					headerTitle: () => (
+						<Image
+							source={Logo}
+							style={{
+								width: 150,
+								height: 50,
+								resizeMode: "contain",
+							}}
+						/>
+					),
+				}}
+			>
+				<Pilha.Screen name="Index" component={Index} />
+			</Pilha.Navigator>
+		</NavigationContainer>
+	);
+}
