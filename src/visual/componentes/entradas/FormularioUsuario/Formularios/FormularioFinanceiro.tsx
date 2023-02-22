98eae61a25293b3f8a3a9ff605dc5dd31045a809
Rodrigo <rod_igomail@hotmail.com>
2023-02-22 11:35:00 -0300

    finalizada aula 'Formulário de Dados Financeiros'

--- src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioFinanceiro.tsx
+++ src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioFinanceiro.tsx
@@ -1,11 +1,27 @@
 import { View } from "react-native";
 
-// import { ContextoUsuario } from "logica/contextos/ContextoUsuario";
-// import { useContext } from "react";
-// import { useFormContext } from "react-hook-form";
+import { ContextoUsuario } from "logica/contextos/ContextoUsuario";
+import { useContext } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import CampoDeTexto from "../../CampoDeTexto/CampoDeTexto";
 
 export const FormularioFinanceiro: React.FC = () => {
-	// const { register } = useFormContext(),
-	// 	{ usuario } = useContext(ContextoUsuario).estadoUsuario;
-	return <View></View>;
+	const { control } = useFormContext<{ usuario: { chave_pix: string } }>(),
+		{ usuario } = useContext(ContextoUsuario).estadoUsuario;
+	return (
+		<View>
+			<Controller
+				name={"usuario.chave_pix"}
+				defaultValue={usuario.chave_pix}
+				control={control}
+				render={({ field }) => (
+					<CampoDeTexto
+						label={"Chave Pix"}
+						value={field.value}
+						onChangeText={(valor) => field.onChange(valor)}
+					/>
+				)}
+			/>
+		</View>
+	);
 };
