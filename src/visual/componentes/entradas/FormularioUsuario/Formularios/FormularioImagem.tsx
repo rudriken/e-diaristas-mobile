d3f6f70807eec9337e508672e26c352660b7f4ed
Rodrigo <rod_igomail@hotmail.com>
2023-02-22 11:03:45 -0300

    finalizada aula 'Formulário de Foto do Documento'

--- src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioImagem.tsx
+++ src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioImagem.tsx
@@ -1,10 +1,23 @@
 import { View } from "react-native";
-
-// import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
+import CampoDeArquivo from "../../CampoDeArquivo/CampoDeArquivo";
 
 export const FormularioImagem: React.FC = () => {
-	// const { control } = useFormContext<{
-	// 	usuario: { foto_documento: string };
-	// }>();
-	return <View></View>;
+	const { control } = useFormContext<{
+		usuario: { foto_documento: string };
+	}>();
+	return (
+		<View>
+			<Controller
+				name={"usuario.foto_documento"}
+				defaultValue={""}
+				control={control}
+				render={({ field }) => (
+					<CampoDeArquivo
+						aoAlterar={(arquivo) => field.onChange(arquivo)}
+					/>
+				)}
+			/>
+		</View>
+	);
 };
