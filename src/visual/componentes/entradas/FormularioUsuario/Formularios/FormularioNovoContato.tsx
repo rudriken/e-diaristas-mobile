ddc9efbc61dbd6f28aa9831ca77fd53f579aba58
Rodrigo <rod_igomail@hotmail.com>
2023-02-23 09:26:18 -0300

    finalizada aula 'Formulário de Novo Cadastro'

--- src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioNovoContato.tsx
+++ src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioNovoContato.tsx
@@ -1,19 +1,76 @@
 import { View } from "react-native";
-
-// import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
+import CampoDeTexto from "../../CampoDeTexto/CampoDeTexto";
 
 export const FormularioNovoContato: React.FC = () => {
-	// const {
-	// 	register,
-	// 	formState: { errors },
-	// 	watch,
-	// } = useFormContext<{
-	// 	usuario: {
-	// 		email: string;
-	// 		password: string;
-	// 		password_confirmation: string;
-	// 	};
-	// }>();
-	// const novaSenha = watch("usuario.password");
-	return <View></View>;
+	const {
+		control,
+		formState: { errors },
+	} = useFormContext<{
+		usuario: {
+			email: string;
+			password: string;
+			password_confirmation: string;
+		};
+	}>();
+	return (
+		<View>
+			<Controller
+				control={control}
+				name={"usuario.email"}
+				defaultValue={""}
+				render={({ field }) => {
+					return (
+						<CampoDeTexto
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"E-mail"}
+							keyboardType={"email-address"}
+							error={errors?.usuario?.email !== undefined}
+							textoDeAjuda={errors?.usuario?.email?.message}
+						/>
+					);
+				}}
+			/>
+			<Controller
+				control={control}
+				name={"usuario.password"}
+				defaultValue={""}
+				render={({ field }) => {
+					return (
+						<CampoDeTexto
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"Senha"}
+							secureTextEntry
+							error={errors?.usuario?.password !== undefined}
+							textoDeAjuda={errors?.usuario?.password?.message}
+						/>
+					);
+				}}
+			/>
+			<Controller
+				control={control}
+				name={"usuario.password_confirmation"}
+				defaultValue={""}
+				render={({ field }) => {
+					return (
+						<CampoDeTexto
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"Confirmação da Senha"}
+							secureTextEntry
+							error={
+								errors?.usuario?.password_confirmation !==
+								undefined
+							}
+							textoDeAjuda={
+								errors?.usuario?.password_confirmation?.message
+							}
+						/>
+					);
+				}}
+			/>
+		</View>
+	);
 };
