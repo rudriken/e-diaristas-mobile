dc93186dcfc3c40b404c984cacae3a24184f2788
Rodrigo <rod_igomail@hotmail.com>
2023-02-23 10:27:04 -0300

    finalizada aula 'Formulário de Dados do Usuário'

--- src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioDadosUsuario.tsx
+++ src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioDadosUsuario.tsx
@@ -1,8 +1,10 @@
+import { useContext } from "react";
 import { View } from "react-native";
-
-// import { useFormContext } from "react-hook-form";
-
-// import { ContextoUsuario } from "logica/contextos/ContextoUsuario";
+import { Controller, useFormContext } from "react-hook-form";
+import { ContextoUsuario } from "logica/contextos/ContextoUsuario";
+import CampoDeTexto from "../../CampoDeTexto/CampoDeTexto";
+import { ServicoFormatadorDeTexto } from "logica/servicos/ServicoFormatadorDeTexto";
+import CampoDeTextoComMascara from "../../CampoDeTextoComMascara/CampoDeTextoComMascara";
 
 export interface FormularioDadosUsuarioProps {
 	cadastro?: boolean;
@@ -11,18 +13,95 @@ export interface FormularioDadosUsuarioProps {
 export const FormularioDadosUsuario: React.FC<FormularioDadosUsuarioProps> = ({
 	cadastro = false,
 }) => {
-	// const {
-	// 		register,
-	// 		formState: { errors },
-	// 		control,
-	// 	} = useFormContext<{
-	// 		usuario: {
-	// 			nome_completo: string;
-	// 			nascimento: string;
-	// 			cpf: string;
-	// 			telefone: string;
-	// 		};
-	// 	}>(),
-
-	return <View></View>;
+	const {
+		formState: { errors },
+		control,
+	} = useFormContext<{
+		usuario: {
+			nome_completo: string;
+			nascimento: string;
+			cpf: string;
+			telefone: string;
+		};
+	}>();
+	const { usuario } = useContext(ContextoUsuario).estadoUsuario;
+	return (
+		<View>
+			<Controller
+				control={control}
+				name={"usuario.nome_completo"}
+				defaultValue={usuario.nome_completo}
+				render={({ field }) => {
+					return (
+						<CampoDeTexto
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"Nome completo"}
+							error={errors?.usuario?.nome_completo !== undefined}
+							textoDeAjuda={
+								errors?.usuario?.nome_completo?.message
+							}
+						/>
+					);
+				}}
+			/>
+			<Controller
+				control={control}
+				name={"usuario.nascimento"}
+				defaultValue={ServicoFormatadorDeTexto.reverterFormatoDeData(
+					usuario.nascimento as string
+				)}
+				render={({ field }) => {
+					return (
+						<CampoDeTextoComMascara
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"Data de nascimento"}
+							mascara={"99/99/9999"}
+							keyboardType={"number-pad"}
+							error={errors?.usuario?.nascimento !== undefined}
+							textoDeAjuda={errors?.usuario?.nascimento?.message}
+						/>
+					);
+				}}
+			/>
+			<Controller
+				control={control}
+				name={"usuario.cpf"}
+				defaultValue={usuario.cpf}
+				render={({ field }) => {
+					return (
+						<CampoDeTextoComMascara
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"CPF"}
+							mascara={"999.999.999-99"}
+							keyboardType={"number-pad"}
+							error={errors?.usuario?.cpf !== undefined}
+							textoDeAjuda={errors?.usuario?.cpf?.message}
+							disabled={!cadastro}
+						/>
+					);
+				}}
+			/>
+			<Controller
+				control={control}
+				name={"usuario.telefone"}
+				defaultValue={usuario.telefone}
+				render={({ field }) => {
+					return (
+						<CampoDeTextoComMascara
+							value={field.value}
+							onChangeText={(valor) => field.onChange(valor)}
+							label={"Telefone"}
+							mascara={"(99) 99999-9999"}
+							keyboardType={"number-pad"}
+							error={errors?.usuario?.telefone !== undefined}
+							textoDeAjuda={errors?.usuario?.telefone?.message}
+						/>
+					);
+				}}
+			/>
+		</View>
+	);
 };
