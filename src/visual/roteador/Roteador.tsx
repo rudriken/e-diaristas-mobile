4de4f584bd1f9ff03d037ad882e342840aa242c6
Rodrigo <rod_igomail@hotmail.com>
2023-03-03 17:00:10 -0300

    finalizada aula 'Navegação por Abas'

--- src/visual/roteador/Roteador.tsx
+++ src/visual/roteador/Roteador.tsx
@@ -1,22 +1,39 @@
 import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Logo from "@assets/img/logos/e-diaristas-logo.png";
 import { NavigationTema } from "visual/temas/app-tema";
 import Index from "pages";
 import EncontrarDiarista from "pages/encontrar-diarista";
+import IconeDeFonte from "visual/componentes/exibe-dados/IconeDeFonte/IconeDeFonte";
 
-export type listaDeParametrosDaPilhaRaiz = {
+// export type listaDeParametrosDaPilhaRaiz = {
+// 	Index: undefined;
+// 	EncontrarDiarista: undefined;
+// };
+
+export type listaDeParametrosDaGuiaInferiorRaiz = {
 	Index: undefined;
 	EncontrarDiarista: undefined;
 };
 
-const Pilha = createStackNavigator<listaDeParametrosDaPilhaRaiz>();
+// const Pilha = createStackNavigator<listaDeParametrosDaPilhaRaiz>();
+const GuiaInferior =
+	createBottomTabNavigator<listaDeParametrosDaGuiaInferiorRaiz>();
+
+function pegarIcone(
+	nomeDoIcone: TwIcon
+): (propriedades: { color: string; size: number }) => JSX.Element {
+	return ({ color, size }) => {
+		return <IconeDeFonte icone={nomeDoIcone} cor={color} tamanho={size} />;
+	};
+}
 
 export default function Roteador() {
 	return (
 		<NavigationContainer theme={NavigationTema}>
-			<Pilha.Navigator
+			<GuiaInferior.Navigator
 				screenOptions={{
 					headerTitleAlign: "center",
 					headerTitle: () => (
@@ -31,12 +48,17 @@ export default function Roteador() {
 					),
 				}}
 			>
-				<Pilha.Screen name="Index" component={Index} />
-				<Pilha.Screen
+				<GuiaInferior.Screen name="Index" component={Index} />
+				<GuiaInferior.Screen
 					name="EncontrarDiarista"
 					component={EncontrarDiarista}
+					options={{
+						title: "Encontrar Diaristas",
+						tabBarIcon: pegarIcone("bars"),
+						tabBarBadge: 15,
+					}}
 				/>
-			</Pilha.Navigator>
+			</GuiaInferior.Navigator>
 		</NavigationContainer>
 	);
 }
