4117444d08650c1bdbb7ebf720c0c020b0997830
Rodrigo <rod_igomail@hotmail.com>
2023-03-15 10:14:45 -0300

    finalizada aula 'Preparação da Tela e Parcial de Diárias'

--- src/pages/diarias.tsx
+++ src/pages/diarias.tsx
@@ -1,7 +1,12 @@
-import { Text } from "react-native";
+import { ProvedorDiaria } from "logica/contextos/ContextoDiarias";
+import MinhasDiarias from "@parciais/diarias/_minhas-diarias";
 
 const Diarias = () => {
-	return <Text>Tela 'Diarias'</Text>;
+	return (
+		<ProvedorDiaria>
+			<MinhasDiarias />
+		</ProvedorDiaria>
+	);
 };
 
 export default Diarias;
---
+++ src/visual/parciais/diarias/_minhas-diarias.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const MinhasDiarias = () => {
+	return <Text>Tela 'Minhas Diárias'</Text>;
+};
+
+export default MinhasDiarias;
