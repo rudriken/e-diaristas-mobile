8bda18a7d8099f9237e8cbb9ed0538dc71c9f221
Rodrigo <rod_igomail@hotmail.com>
2023-03-15 10:36:55 -0300

    finalizada aula 'Criação da Tela e Botões de Filtro'

--- src/visual/parciais/diarias/_minhas-diarias.tsx
+++ src/visual/parciais/diarias/_minhas-diarias.tsx
@@ -1,7 +1,46 @@
-import { Text } from "react-native";
+import { ScrollView } from "react-native";
+import useMinhasDiarias from "logica/ganchos/pages/diarias/useMinhasDiarias.page";
+import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
+import Botao from "visual/componentes/entradas/Botao/Botao";
+import { BotoesContainer } from "./_minhas-diarias.styled";
 
 const MinhasDiarias = () => {
-	return <Text>Tela 'Minhas Diárias'</Text>;
+	const { filtro, modificarFiltro } = useMinhasDiarias();
+
+	return (
+		<ScrollView>
+			<TituloPagina titulo={"Minhas Diárias"} />
+			<BotoesContainer>
+				<Botao
+					uppercase={false}
+					dark
+					onPress={() => modificarFiltro("pendentes")}
+					mode={filtro === "pendentes" ? "contained" : "outlined"}
+					style={{ width: 133 }}
+				>
+					Pendentes
+				</Botao>
+				<Botao
+					uppercase={false}
+					dark
+					onPress={() => modificarFiltro("avaliadas")}
+					mode={filtro === "avaliadas" ? "contained" : "outlined"}
+					style={{ width: 133 }}
+				>
+					Avaliadas
+				</Botao>
+				<Botao
+					uppercase={false}
+					dark
+					onPress={() => modificarFiltro("canceladas")}
+					mode={filtro === "canceladas" ? "contained" : "outlined"}
+					style={{ width: 133 }}
+				>
+					Canceladas
+				</Botao>
+			</BotoesContainer>
+		</ScrollView>
+	);
 };
 
 export default MinhasDiarias;
