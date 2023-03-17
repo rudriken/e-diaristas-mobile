5a73c7796fba0d9e93bb31023b1936f9483ec694
Rodrigo <rod_igomail@hotmail.com>
2023-03-17 13:07:02 -0300

    finalizada aula 'Formulário de Cadastro'

--- src/pages/cadastro/diarista.tsx
+++ src/pages/cadastro/diarista.tsx
@@ -1,15 +1,32 @@
 import { useEffect, useRef } from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { FormProvider } from "react-hook-form";
 import { ServicoMovel } from "logica/servicos/ServicoMovel";
 import useCadastroDiarista from "logica/ganchos/pages/cadastro/useCadastroDiarista.page";
 import { useTheme } from "@emotion/react";
 import MigalhaDePao from "visual/componentes/navegacao/MigalhaDePao/MigalhaDePao";
 import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
-import { FormularioUsuarioContainer } from "visual/componentes/entradas/FormularioUsuario/FormularioUsuario";
+import {
+	FormularioDadosUsuario,
+	FormularioEndereco,
+	FormularioFinanceiro,
+	FormularioImagem,
+	FormularioNovoContato,
+	FormularioUsuarioContainer,
+	TituloDoGrupoDeCampoFormulario,
+} from "visual/componentes/entradas/FormularioUsuario/FormularioUsuario";
+import { Paragraph } from "react-native-paper";
+import Botao from "visual/componentes/entradas/Botao/Botao";
 
 const Diarista = () => {
 	const scrollViewRef = useRef<ScrollView>(null),
-		{ passo, migalhaDePaoItens } = useCadastroDiarista(),
+		{
+			passo,
+			esperandoResposta,
+			migalhaDePaoItens,
+			formularioUsuario,
+			aoSubmeterUsuario,
+		} = useCadastroDiarista(),
 		cores = useTheme().colors;
 
 	useEffect(() => {
@@ -43,7 +60,66 @@ const Diarista = () => {
 				/>
 			)}
 
-			<FormularioUsuarioContainer></FormularioUsuarioContainer>
+			<FormularioUsuarioContainer>
+				<View style={{ display: passo !== 1 ? "none" : "flex" }}>
+					<FormProvider {...formularioUsuario}>
+						<TituloDoGrupoDeCampoFormulario
+							style={{ marginTop: 0 }}
+						>
+							Dados pessoais
+						</TituloDoGrupoDeCampoFormulario>
+						<FormularioDadosUsuario cadastro />
+
+						<TituloDoGrupoDeCampoFormulario>
+							Financeiro
+						</TituloDoGrupoDeCampoFormulario>
+						<FormularioFinanceiro />
+
+						<TituloDoGrupoDeCampoFormulario>
+							Hora da self! Envie uma self segurando o documento
+						</TituloDoGrupoDeCampoFormulario>
+						<Paragraph
+							style={{
+								marginTop: -16,
+								marginBottom: 16,
+								textAlign: "center",
+							}}
+						>
+							Para sua segurança, todos os profissionais e
+							clientes precisam enviar. Essa foto não será vista
+							por ninguém.
+						</Paragraph>
+						<FormularioImagem />
+
+						<TituloDoGrupoDeCampoFormulario>
+							Endereço
+						</TituloDoGrupoDeCampoFormulario>
+						<FormularioEndereco />
+
+						<TituloDoGrupoDeCampoFormulario>
+							Dados de acesso
+						</TituloDoGrupoDeCampoFormulario>
+						<FormularioNovoContato />
+
+						<View>
+							<Botao
+								uppercase={false}
+								dark
+								buttonColor={cores.accent}
+								mode={"contained"}
+								onPress={formularioUsuario.handleSubmit(
+									aoSubmeterUsuario
+								)}
+								larguraTotal
+								style={{ marginTop: 32, marginBottom: 24 }}
+								disabled={esperandoResposta}
+							>
+								Cadastrar e escolher cidades
+							</Botao>
+						</View>
+					</FormProvider>
+				</View>
+			</FormularioUsuarioContainer>
 		</ScrollView>
 	);
 };
