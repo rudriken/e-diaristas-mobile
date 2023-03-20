46f43a515ba5fcecd94211d5c55b1120442a36a4
Rodrigo <rod_igomail@hotmail.com>
2023-03-20 09:27:53 -0300

    finalizada aula 'Criação da Tela'

--- src/pages/pagamentos.tsx
+++ src/pages/pagamentos.tsx
@@ -1,7 +1,33 @@
-import { Text } from "react-native";
+import { ScrollView } from "react-native";
+import usePagamentos from "logica/ganchos/pages/usePagamentos.page";
+import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
+import { BotoesContainer } from "@parciais/diarias/_minhas-diarias.styled";
+import Botao from "visual/componentes/entradas/Botao/Botao";
+import { Paragraph } from "react-native-paper";
 
 const Pagamentos = () => {
-	return <Text>Tela 'Pagamentos'</Text>;
+	const { dadosFiltrados } = usePagamentos();
+	return (
+		<ScrollView>
+			<TituloPagina titulo={"Pagamentos"} />
+			<BotoesContainer>
+				<Botao uppercase={false} dark>
+					Pago
+				</Botao>
+				<Botao uppercase={false} dark>
+					Aguardando Transferência
+				</Botao>
+			</BotoesContainer>
+
+			{dadosFiltrados.length > 0 ? (
+				<></>
+			) : (
+				<Paragraph style={{ paddingTop: 80, textAlign: "center" }}>
+					Nenhum pagamento ainda
+				</Paragraph>
+			)}
+		</ScrollView>
+	);
 };
 
 export default Pagamentos;
