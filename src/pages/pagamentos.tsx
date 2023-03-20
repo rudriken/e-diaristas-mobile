49a54cc5ebd01b2521f4da4185834ccb442adfb8
Rodrigo <rod_igomail@hotmail.com>
2023-03-20 09:53:52 -0300

    finalizada aula 'Listagem dos Dados'

--- src/pages/pagamentos.tsx
+++ src/pages/pagamentos.tsx
@@ -1,9 +1,12 @@
-import { ScrollView } from "react-native";
-import usePagamentos from "logica/ganchos/pages/usePagamentos.page";
-import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
-import { BotoesContainer } from "@parciais/diarias/_minhas-diarias.styled";
-import Botao from "visual/componentes/entradas/Botao/Botao";
+import { ScrollView, Text } from "react-native";
 import { Paragraph } from "react-native-paper";
+import usePagamentos from "logica/ganchos/pages/usePagamentos.page";
+import { ServicoPagamento } from "logica/servicos/ServicoPagamento";
+import { ServicoFormatadorDeTexto } from "logica/servicos/ServicoFormatadorDeTexto";
+import ListaDeDados from "visual/componentes/exibe-dados/ListaDeDados/ListaDeDados";
+import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
+import Botao from "visual/componentes/entradas/Botao/Botao";
+import { BotoesContainer } from "@parciais/diarias/_minhas-diarias.styled";
 
 const Pagamentos = () => {
 	const { dadosFiltrados } = usePagamentos();
@@ -19,8 +22,48 @@ const Pagamentos = () => {
 				</Botao>
 			</BotoesContainer>
 
-			{dadosFiltrados.length > 0 ? (
-				<></>
+			{true ? (
+				<>
+					{dadosFiltrados.map((item) => {
+						return (
+							<ListaDeDados
+								key={item.id}
+								cabecalho={
+									<Text>
+										Data:{" "}
+										{ServicoFormatadorDeTexto.reverterFormatoDeData(
+											item.created_at
+										)}
+									</Text>
+								}
+								corpo={
+									<>
+										<Text style={{ color: "white" }}>
+											Status:{" "}
+											{
+												ServicoPagamento.pegarStatus(
+													item.status
+												).rotulo
+											}
+										</Text>
+										<Text style={{ color: "white" }}>
+											Valor diária:{" "}
+											{ServicoFormatadorDeTexto.formatarMoeda(
+												item.valor
+											)}
+										</Text>
+										<Text style={{ color: "white" }}>
+											Valor depósito:{" "}
+											{ServicoFormatadorDeTexto.formatarMoeda(
+												item.valor_deposito
+											)}
+										</Text>
+									</>
+								}
+							/>
+						);
+					})}
+				</>
 			) : (
 				<Paragraph style={{ paddingTop: 80, textAlign: "center" }}>
 					Nenhum pagamento ainda
