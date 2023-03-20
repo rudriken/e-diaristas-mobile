57b920232baff2c34447471ef88895adbfea36b3
Rodrigo <rod_igomail@hotmail.com>
2023-03-20 10:14:24 -0300

    finalizada aula 'Criação da Tela' 'Oportunidades'

--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { useTheme } from "@emotion/react";
+import { ScrollView } from "react-native";
+import useOportunidadesTrabalho from "logica/ganchos/pages/useOportunidades.page";
+import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
 
 const Oportunidades = () => {
-	return <Text>Tela 'Oportunidades'</Text>;
+	const cores = useTheme().colors,
+		{} = useOportunidadesTrabalho();
+	return (
+		<ScrollView>
+			<TituloPagina titulo={"Oportunidades de trabalho"} />
+		</ScrollView>
+	);
 };
 
 export default Oportunidades;
