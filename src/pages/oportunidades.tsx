ea150b2e8d8309c4d0467ea05be080365b61504c
Rodrigo <rod_igomail@hotmail.com>
2023-03-21 11:40:45 -0300

    finalizada aula 'Criação da Tela' (revisão 1)

--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -1,27 +1,11 @@
-import { useTheme } from "@emotion/react";
 import { ScrollView } from "react-native";
+import { useTheme } from "@emotion/react";
 import useOportunidadesTrabalho from "logica/ganchos/pages/useOportunidades.page";
 import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
-import { Paragraph } from "react-native-paper";
-import ListaDeDados from "visual/componentes/exibe-dados/ListaDeDados/ListaDeDados";
 
 const Oportunidades = () => {
-	const cores = useTheme().colors,
-		{
-			movel,
-			oportunidades,
-			paginaAtual,
-			alterarPaginaAtual,
-			totalPaginas,
-			itensPorPagina,
-			oportunidadeSelecionada,
-			alterarOportunidadeSelecionada,
-			seCandidatar,
-			mensagemFeedback,
-			alterarMensagemFeedback,
-			totalComodos,
-			podeCandidatar,
-		} = useOportunidadesTrabalho();
+	const cores = useTheme().colors;
+	const {} = useOportunidadesTrabalho();
 	return (
 		<ScrollView>
 			<TituloPagina titulo={"Oportunidades de trabalho"} />
