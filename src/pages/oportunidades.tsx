929a9be3166b79b465cb89aef4acfb2bb1df9a55
Rodrigo <rod_igomail@hotmail.com>
2023-03-21 10:36:29 -0300

    problema: constante 'oportunidades' não recebe um objeto iterável (enviado para suporte)

--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -2,13 +2,42 @@ import { useTheme } from "@emotion/react";
 import { ScrollView } from "react-native";
 import useOportunidadesTrabalho from "logica/ganchos/pages/useOportunidades.page";
 import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
+import { Paragraph } from "react-native-paper";
+import ListaDeDados from "visual/componentes/exibe-dados/ListaDeDados/ListaDeDados";
 
 const Oportunidades = () => {
 	const cores = useTheme().colors,
-		{} = useOportunidadesTrabalho();
+		{
+			movel,
+			oportunidades,
+			paginaAtual,
+			alterarPaginaAtual,
+			totalPaginas,
+			itensPorPagina,
+			oportunidadeSelecionada,
+			alterarOportunidadeSelecionada,
+			seCandidatar,
+			mensagemFeedback,
+			alterarMensagemFeedback,
+			totalComodos,
+			podeCandidatar,
+		} = useOportunidadesTrabalho();
+	console.log(oportunidades);
 	return (
 		<ScrollView>
 			<TituloPagina titulo={"Oportunidades de trabalho"} />
+
+			{oportunidades.length > 0 ? (
+				<>
+					{oportunidades.map((item) => {
+						return <ListaDeDados />;
+					})}
+				</>
+			) : (
+				<Paragraph style={{ paddingTop: 80, textAlign: "center" }}>
+					Nenhuma oportunidade ainda
+				</Paragraph>
+			)}
 		</ScrollView>
 	);
 };
