35801e7d713dc8f5f450567c44a7b66da8bbc3e5
Rodrigo <rod_igomail@hotmail.com>
2023-03-21 11:36:48 -0300

    finalizada aula 'Filtros' (revisão 1))

--- src/pages/pagamentos.tsx
+++ src/pages/pagamentos.tsx
@@ -1,29 +1,39 @@
 import { ScrollView, View, Text } from "react-native";
-import usePagamentos from "logica/ganchos/pages/usePagamentos.page";
-import { BotoesContainer } from "@parciais/diarias/_minhas-diarias.styled";
 import { Paragraph } from "react-native-paper";
+import usePagamentos from "logica/ganchos/pages/usePagamentos.page";
+import { ServicoFormatadorDeTexto } from "logica/servicos/ServicoFormatadorDeTexto";
+import { ServicoPagamento } from "logica/servicos/ServicoPagamento";
+import { BotoesContainer } from "@parciais/diarias/_minhas-diarias.styled";
 import Botao from "visual/componentes/entradas/Botao/Botao";
 import ListaDeDados from "visual/componentes/exibe-dados/ListaDeDados/ListaDeDados";
 import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
-import { ServicoFormatadorDeTexto } from "logica/servicos/ServicoFormatadorDeTexto";
-import { ServicoPagamento } from "logica/servicos/ServicoPagamento";
 
 const Pagamentos = () => {
-	const { dadosFiltrados } = usePagamentos();
+	const { dadosFiltrados, filtro, modificarFiltro } = usePagamentos();
 	// console.log("dadosFiltrados:", dadosFiltrados);
 	return (
 		<ScrollView>
 			<TituloPagina titulo={"Pagamentos"} />
 			<BotoesContainer>
-				<Botao uppercase={false} dark>
+				<Botao
+					uppercase={false}
+					dark
+					onPress={() => modificarFiltro("pago")}
+					mode={filtro === "pago" ? "contained" : "outlined"}
+				>
 					Pago
 				</Botao>
-				<Botao uppercase={false} dark>
+				<Botao
+					uppercase={false}
+					dark
+					onPress={() => modificarFiltro("aguardando")}
+					mode={filtro === "aguardando" ? "contained" : "outlined"}
+				>
 					Aguardando Transferência
 				</Botao>
 			</BotoesContainer>
 
-			{true ? (
+			{dadosFiltrados.length > 0 ? (
 				<>
 					{dadosFiltrados.map((item) => {
 						return (
