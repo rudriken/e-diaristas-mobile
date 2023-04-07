ae4b040a6e87bdd08d2690e3339a19aed93ce810
Rodrigo <rod_igomail@hotmail.com>
2023-04-07 11:20:36 -0300

    finalizada aula 'Modal de Oportunidade e Confirmação'

--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -3,11 +3,14 @@ import useOportunidadesTrabalho from "logica/ganchos/pages/useOportunidades.page
 import { ScrollView, View, Text } from "react-native";
 import { ServicoFormatadorDeTexto } from "logica/servicos/ServicoFormatadorDeTexto";
 import { stringParaObjeto } from "logica/servicos/funcoesReparadoras";
-import { Paragraph } from "react-native-paper";
+import { Caption, Paragraph } from "react-native-paper";
 import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
 import ListaDeDados from "visual/componentes/exibe-dados/ListaDeDados/ListaDeDados";
 import Botao from "visual/componentes/entradas/Botao/Botao";
 import Dialogo from "visual/componentes/retorno/Dialogo/Dialogo";
+import { CaixinhaDeTrabalho } from "@parciais/diarias/_minhas-diarias-dialogos";
+import { DiariaInterface } from "logica/@tipos/DiariaInterface";
+import InformacaoDoUsuario from "visual/componentes/exibe-dados/InformacaoDoUsuario/InformacaoDoUsuario";
 
 const Oportunidades = () => {
 	const cores = useTheme().colors;
@@ -17,6 +20,7 @@ const Oportunidades = () => {
 		oportunidadeSelecionada,
 		alterarOportunidadeSelecionada,
 		podeCandidatar,
+		seCandidatar,
 	} = useOportunidadesTrabalho();
 
 	let oportunidadesC = oportunidades;
@@ -94,7 +98,77 @@ const Oportunidades = () => {
 				<Dialogo
 					aberto={true}
 					aoFechar={() => alterarOportunidadeSelecionada(undefined)}
-				></Dialogo>
+					subtitulo={
+						"Tem certeza que deseja se candidatar à diária abaixo?"
+					}
+					aoConfirmar={() =>
+						oportunidadeSelecionada &&
+						seCandidatar(oportunidadeSelecionada)
+					}
+				>
+					<ScrollView>
+						<CaixinhaDeTrabalho
+							diaria={oportunidadeSelecionada as DiariaInterface}
+						/>
+						<InformacaoDoUsuario
+							nome={
+								oportunidadeSelecionada?.cliente
+									.nome_completo || ""
+							}
+							avaliacao={
+								oportunidadeSelecionada?.cliente.reputacao || 0
+							}
+							foto={
+								oportunidadeSelecionada?.cliente.foto_usuario ||
+								""
+							}
+						/>
+						{oportunidadeSelecionada !== undefined &&
+							oportunidadeSelecionada?.avaliacoes_cliente.length >
+								0 && (
+								<ListaDeDados
+									cabecalho={
+										<Text>
+											Últimas avaliações do clien-te
+										</Text>
+									}
+									corpo={
+										<>
+											{oportunidadeSelecionada?.avaliacoes_cliente.map(
+												(item, indice) => {
+													return (
+														<InformacaoDoUsuario
+															key={indice}
+															nome={
+																item.nome_avaliador
+															}
+															foto={
+																item.foto_avaliador
+															}
+															avaliacao={
+																item.nota
+															}
+															descricao={
+																item.descricao
+															}
+														/>
+													);
+												}
+											)}
+										</>
+									}
+								/>
+							)}
+						<Caption style={{ paddingTop: 16, paddingBottom: 16 }}>
+							Ao se candidatar você ainda não é o(a) diarista
+							escolhido(a) para realizar o trabalho. Vamos
+							analisar suas qualificações e a distância para o
+							local da diária. Caso você seja a pessoa
+							selecionada, receberá um e-mail avisando. Atente-se
+							à sua caixa de entrada!
+						</Caption>
+					</ScrollView>
+				</Dialogo>
 			)}
 		</ScrollView>
 	);
